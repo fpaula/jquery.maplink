f674788c9f70fbd8dd587cff22e74b178b958efa
Fred <frederico.paula@abril.com.br>
2011-11-01 16:09:20 -0200

    JSLint validation

--- jquery.maplink.js
+++ jquery.maplink.js
@@ -1,27 +1,30 @@
-(function($){
-    $.fn.maplink = function(settings){
-        var config = {
-			latitude: -23.5489433,
-			longitude: -46.6388182,
-			zoom: 4,
-			infoWindowHtml: ''
-        };
-		var map = null;
-		var routeManager = null;
-        if (settings){$.extend(config, settings);}
+(function ($) {
+    'use strict';
+    $.fn.maplink = function (settings) {
+        var map = null,
+            config = {
+                latitude: -23.5489433,
+                longitude: -46.6388182,
+                zoom: 4,
+                infoWindowHtml: ''
+            };
+        if (settings) {
+            $.extend(config, settings);
+        }
 
-        return this.each(function(){
+        return this.each(function () {
             map = new MMap2(document.getElementById(this.id));
-			map.addControl(new MMapControls());
-			map.addControl(new GLargeMapControl());
+            map.addControl(new MMapControls());
+            map.addControl(new GLargeMapControl());
 
-			marker = new MMarker(new MPoint(config.longitude, config.latitude));
-	        LBS.Event.addListener(marker, "click", function (e) {
-		    marker.openInfoWindowHtml(config.infoWindowHtml);
-	           LBS.Event.stop(e);
-	        });		
-			map.addMarker(marker);
-			map.setCenter(new MPoint(config.longitude, config.latitude), config.zoom);
+            var marker = new MMarker(new MPoint(config.longitude, config.latitude));
+            LBS.Event.addListener(marker, "click",
+                function (e) {
+                    marker.openInfoWindowHtml(config.infoWindowHtml);
+                    LBS.Event.stop(e);
+                });
+            map.addMarker(marker);
+            map.setCenter(new MPoint(config.longitude, config.latitude), config.zoom);
         });
     };
-})(jQuery);
+}(jQuery));
