294750993eed2ffe868f259447769fa541e47a88
Fred <frederico.paula@abril.com.br>
2011-11-02 18:57:36 -0200

    Add Markers

--- jquery.maplink.js
+++ jquery.maplink.js
@@ -3,10 +3,12 @@
     $.fn.maplink = function (settings) {
         var map = null,
             config = {
-                latitude: -23.5489433,
-                longitude: -46.6388182,
-                zoom: 4,
-                infoWindowHtml: ''
+                latitude: -23.5505233,
+                longitude: -46.6342982,
+                zoom: 10,
+                infoWindowHtml: '',
+				showMarker: true,
+				markers: null
             };
         if (settings) {
             $.extend(config, settings);
@@ -16,15 +18,37 @@
             map = new MMap2(document.getElementById(this.id));
             map.addControl(new MMapControls());
             map.addControl(new GLargeMapControl());
-
-            var marker = new MMarker(new MPoint(config.longitude, config.latitude));
-            LBS.Event.addListener(marker, "click",
-                function (e) {
-                    marker.openInfoWindowHtml(config.infoWindowHtml);
-                    LBS.Event.stop(e);
-                });
-            map.addMarker(marker);
+			if(config.showMarker === true){
+				var marker = new MMarker(new MPoint(config.longitude, config.latitude));
+				LBS.Event.addListener(marker, "click",
+				    function (e) {
+				        marker.openInfoWindowHtml(config.infoWindowHtml);
+				        LBS.Event.stop(e);
+				    });
+				map.addMarker(marker);	
+			}
             map.setCenter(new MPoint(config.longitude, config.latitude), config.zoom);
+			if(config.markers !== null){
+				for(var i in config.markers){
+					var point = new MPoint(config.markers[i].longitude, config.markers[i].latitude);
+					
+					var icon = new MIcon();
+					icon.image = config.markers[i].icon.image;
+					icon.iconSize = new MSize(config.markers[i].icon.dimentions.x, config.markers[i].icon.dimentions.y);
+					icon.infoWindowAnchor = new MIconPoint(5, 5);
+					
+			       	var marker = new MMarker(point, icon);
+			       	LBS.Event.addListener(marker, "click", 
+						function (i) {
+				    		return function (e) {
+								marker.openInfoWindowHtml(config.markers[i].html);
+			           			LBS.Event.stop(e);
+							}
+			       		}(i)
+					);
+					map.addMarker(marker);
+				}
+			}
         });
     };
 }(jQuery));
