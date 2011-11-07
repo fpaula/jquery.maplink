d1ad42b449e8d75393cfc7dafcafd9b5c62e38ce
Fred <frederico.paula@abril.com.br>
2011-11-07 14:16:18 -0200

    Improving the indentation

--- jquery.maplink.js
+++ jquery.maplink.js
@@ -1,5 +1,4 @@
 (function ($) {
-    'use strict';
     $.fn.maplink = function (settings) {
         var map = null,
             config = {
@@ -7,8 +6,8 @@
                 longitude: -46.6342982,
                 zoom: 10,
                 infoWindowHtml: '',
-				showMarker: true,
-				markers: null
+                showMarker: true,
+                markers: null
             };
         if (settings) {
             $.extend(config, settings);
@@ -18,37 +17,34 @@
             map = new MMap2(document.getElementById(this.id));
             map.addControl(new MMapControls());
             map.addControl(new GLargeMapControl());
-			if(config.showMarker === true){
-				var marker = new MMarker(new MPoint(config.longitude, config.latitude));
-				LBS.Event.addListener(marker, "click",
-				    function (e) {
-				        marker.openInfoWindowHtml(config.infoWindowHtml);
-				        LBS.Event.stop(e);
-				    });
-				map.addMarker(marker);	
-			}
+            if (config.showMarker === true) {
+                var marker = new MMarker(new MPoint(config.longitude, config.latitude));
+                LBS.Event.addListener(marker, "click", function (e) {
+                    marker.openInfoWindowHtml(config.infoWindowHtml);
+                    LBS.Event.stop(e);
+                });
+                map.addMarker(marker);
+            }
             map.setCenter(new MPoint(config.longitude, config.latitude), config.zoom);
-			if(config.markers !== null){
-				for(var i in config.markers){
-					var point = new MPoint(config.markers[i].longitude, config.markers[i].latitude);
-					
-					var icon = new MIcon();
-					icon.image = config.markers[i].icon.image;
-					icon.iconSize = new MSize(config.markers[i].icon.dimentions.x, config.markers[i].icon.dimentions.y);
-					icon.infoWindowAnchor = new MIconPoint(5, 5);
-					
-			       	var marker = new MMarker(point, icon);
-			       	LBS.Event.addListener(marker, "click", 
-						function (i) {
-				    		return function (e) {
-								marker.openInfoWindowHtml(config.markers[i].html);
-			           			LBS.Event.stop(e);
-							}
-			       		}(i)
-					);
-					map.addMarker(marker);
-				}
-			}
+            if (config.markers !== null) {
+                for (var i in config.markers) {
+                    var point = new MPoint(config.markers[i].longitude, config.markers[i].latitude);
+
+                    var icon = new MIcon();
+                    icon.image = config.markers[i].icon.image;
+                    icon.iconSize = new MSize(config.markers[i].icon.dimentions.x, config.markers[i].icon.dimentions.y);
+                    icon.infoWindowAnchor = new MIconPoint(5, 5);
+
+                    var marker = new MMarker(point, icon);
+                    LBS.Event.addListener(marker, "click", function (i) {
+                        return function (e) {
+                            marker.openInfoWindowHtml(config.markers[i].html);
+                            LBS.Event.stop(e);
+                        }
+                    }(i));
+                    map.addMarker(marker);
+                }
+            }
         });
     };
 }(jQuery));
